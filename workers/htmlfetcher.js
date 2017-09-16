54deb70fc37970de937696708000c5cd7e328743
Hack Reactor Student <student@hackreactor.com>
2017-09-16 16:37:48 -0700

    Finish minimum requirements for webhistorian

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,25 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var path = require('path');
+var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var url = require('url');
+
+archive.readListOfUrls((urlList) => {
+  var needToDownload = [];
+  for (var i = 0; i < urlList.length; i++) {
+    archive.isUrlArchived(urlList[i], (answer, url) => {
+      if (!answer) {
+        needToDownload.push(url);
+      }
+    });
+  }
+  setTimeout(function() {
+    archive.downloadUrls(needToDownload);
+  }, 5000);
+});
+
+var logString = new Date;
+logString = logString + ' downloaded new links \n';
+
+fs.appendFile(archive.paths.siteAssets + '/downloadLog.txt', logString);
